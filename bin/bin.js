10b25a67b29c6ad455553326a31a2affadcc0292
Tapani Haka <tapani.haka@barona.fi>
2022-01-21 14:00:01 +0200

    Give the bin a chance to work, even if not in all situations

--- bin/bin.js
+++ bin/bin.js
@@ -1,3 +1,11 @@
 #!/usr/bin/env node
+/* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-env node */
+
+try {
+  require('schematar/dist/api').setMongoose(require('mongoose'))
+} catch (err) {
+  // ignoreed
+}
+
 require('schematar/dist/app')
